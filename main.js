29cec940e95a499ac84a2a557161a817b9446c1d
singhanitish611@gmail.com <singhanitish611@gmail.com>
2024-02-03 02:31:04 +0530

    jhh

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log('Hello World!');
+ 
+   var width=screen.width;
+   var height=-80+screen.height;
+ // document.getElementById('contenar').style.marginTop=(height-width)/2+'px';
+   document.getElementById('contenar'). style.height=width+'px';
+   document.getElementById('contenar'). style.width=width+'px';
+  
